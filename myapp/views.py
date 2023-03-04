974f05fd0d93b671ca432163fe20e9d84fea2437
Your Name <you@example.com>
2023-03-04 16:17:23 +0600

    hellooo

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Feature
 # Create your views here.
 def index(request):
-    name='Anel'
-    return render(request,'index.html',{'name':name})
+    feature=Feature()
+    feature.id=1
+    feature.name='Fast'
+    feature.details='Our service is strongly quick'
+    return render(request,'index.html', {'feature':feature})
+def counter(request):
+    words=request.POST['text']
+    amount=len(words.split())
+    return render(request,'counter.html',{'amount':amount})
